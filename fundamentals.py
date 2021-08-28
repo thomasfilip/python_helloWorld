809864f7ff895b0f2b6656ba925d3d3893c6183c
Thomas Bogatzki <thomas.bogatzki@haw-hamburg.de>
2021-08-28 16:10:19 +0200

    Added function_print(), cli_input().

--- fundamentals.py
+++ fundamentals.py
@@ -14,6 +14,9 @@
 # the __name__ variable will be set as __main__ if the module that is being run is the main program.
 # But if the code is importing the module from another module,
 # then the __name__  variable will be set to that module’s name.
+import sys
+
+
 def execute_in_pycharm():
     print("__name__ variable should contain __main__:")
     print(__name__)
@@ -249,10 +252,57 @@ def strings():
         print(f"String"[i])
 
     # Length of a string.
-    print(f"{len('Five!')}")                                    # 5
+    print(f"{len('Five!')}")                        # 5
 
     name = "Alice"
     print(f"{name} is {len(name)} characters long.")
+
+
+# https://docs.python.org/3.9/library/functions.html#print (version 2021-08-28)
+# print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+#
+# Print objects to the text stream file, separated by sep and followed by end.
+# sep, end, file and flush, if present, must be given as keyword arguments.
+#
+# All non-keyword arguments are converted to strings like str() does and written to the stream,
+# separated by sep and followed by end.
+# Both sep and end must be strings; they can also be None, which means to use the default values.
+# If no objects are given, print() will just write end.
+#
+# The file argument must be an object with a write(string) method;
+# if it is not present or None, sys.stdout will be used. Since printed arguments are converted to text strings,
+# print() cannot be used with binary mode file objects. For these, use file.write(...) instead.
+#
+# Whether output is buffered is usually determined by file,
+# but if the flush keyword argument is true, the stream is forcibly flushed.
+#
+# Changed in version 3.3: Added the flush keyword argument.
+def function_print():
+    print("")
+
+    # Print multiple objects at once.
+    str1 = "Hello"
+    str2 = "Python"
+    print(str1, str2)
+
+    # Add a seperator (must be String; default: ' ').
+    print(str1, str2, sep=' : ')
+
+    # Add end of line (must be String; default: '\n').
+    print(str1, end=' ')
+    print(str2, end=' EOL\n')                       # Hello Python EOL
+
+    # Change the output 'file (must have write(string) method; default: 'sys.stdout').
+    print(str1, str2, file=sys.stdout)
+
+    # Control flush behaviour of streams (True/False; default: False).
+    print(str1, str2, flush=True)
+
+
+# Simple way to get input data from console.
+def cli_input():
+    var_input = input("Enter some data: ")
+    print(f"{var_input}")
 
 
 def loops():
@@ -266,6 +316,8 @@ if __name__ == '__main__':
     boolean_operators()
     truth_value_testing()
     equality_identity()
-    # strings()
+    strings()
+    operator_precedence()
+    function_print()
+    cli_input()
     # loops()
-    # operator_precedence()
