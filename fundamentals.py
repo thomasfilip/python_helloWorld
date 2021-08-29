32ba59b929836353d5c77ea77eecc142813290b1
Thomas Bogatzki <thomas.bogatzki@haw-hamburg.de>
2021-08-29 15:52:36 +0200

    Added data structure tupel.

--- fundamentals.py
+++ fundamentals.py
@@ -418,16 +418,48 @@ def data_structure_list():
     print(f"{42}")
 
 
+def data_structure_tupel():
+    # Tuples are like lists but are immutable.
+    t1 = (1, 2, 3)
+    print(f"{t1[0]}")                                # => 1
+    # t1[0] = 3                                      # TypeError: 'tuple' object does not support item assignment
+
+    # Note that a tuple of length one has to have a comma after the last element but
+    # tuples of other lengths, even zero, do not.
+    print(f"{type((1))}")                           # => <class 'int'>
+    print(f"{type((1,))}")                          # => <class 'tuple'>
+    print(f"{type(())}")                            # => <class 'tuple'>
+
+    # You can do most of the list operations on tuples too.
+    print(f"{len(t1)}")                             # => 3
+    print(f"{t1 + (4, 5, 6)}")                      # => (1, 2, 3, 4, 5, 6)
+    print(f"{t1[:2]}")                              # => (1, 2)
+    print(f"{2 in t1}")                             # => True
+    print(f"{t1[::2]}")                             # => (1, 3)
+    print(f"{t1.index(3)}")                         # => 2
+
+    # You can unpack tuples (or lists) into variables
+    a, b, c = (1, 2, 3)                             # a is now 1, b is now 2 and c is now 3
+    # You can also do extended unpacking
+    a, *b, c = (1, 2, 3, 4)                         # a is now 1, b is now [2, 3] and c is now 4
+    # Tuples are created by default if you leave out the parentheses
+    d, e, f = 4, 5, 6                               # tuple 4, 5, 6 is unpacked into variables d, e and f
+    # respectively such that d = 4, e = 5 and f = 6
+    # Now look how easy it is to swap two values
+    e, d = d, e                                     # d is now 5 and e is now 4
+
+
 if __name__ == '__main__':
-    execute_in_pycharm()
-    primitive_datatypes_and_operators()
-    boolean_operators()
-    truth_value_testing()
-    equality_identity()
-    strings()
-    operator_precedence()
-    function_print()
-    cli_input()
-    control_flow_if_else()
-    loops()
-    data_structure_list()
+    # execute_in_pycharm()
+    # primitive_datatypes_and_operators()
+    # boolean_operators()
+    # truth_value_testing()
+    # equality_identity()
+    # strings()
+    # operator_precedence()
+    # function_print()
+    # cli_input()
+    # control_flow_if_else()
+    # loops()
+    # data_structure_list()
+    data_structure_tupel()
